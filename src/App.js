586d7722a6d6e1a48be9bcc4d83e866e5afc4336
Atsuko Uemura <atsukof.kt@gmail.com>
2024-08-03 18:03:37 -0700

    Add a button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hello World!
         </p>
+        <MyButton />
         <a
           className="App-link"
           href="https://reactjs.org"
